e328ed5799f615a90bce4258f94b70d55eb685e9
Patrick Bader <patrick.bader@thingos.io>
2020-07-19 09:25:49 +0200

    feat: added solutions for previous exercises

--- dungeon_escape/src/main.cpp
+++ dungeon_escape/src/main.cpp
@@ -1,6 +1,49 @@
 #include "helpers.hpp"
+#include "dungeon.hpp"
 
 int main(int argc, char *argv[])
 {
+    Dungeon dungeon;
+
+    std::cout << "You are trapped in a maze, try to escape!\r\n";
+    std::cout << "User 'w', 'a', 's' and 'd' to move.\r\n";
+    dungeon.printVisibleAreaTo(std::cout);
+    getCharacter(); //std::cin.get();
+
+    bool won = false;
+    do
+    {
+        clearScreen(); // std::cout << std::endl;
+
+        dungeon.printVisibleAreaTo(std::cout);
+        char input = getCharacter(); //std::cin.get();
+
+        switch (input)
+        {
+        case 'W':
+        case 'w':
+            won = dungeon.moveUp();
+            break;
+        case 'A':
+        case 'a':
+            won = dungeon.moveLeft();
+            break;
+        case 's':
+        case 'S':
+            won = dungeon.moveDown();
+            break;
+        case 'D':
+        case 'd':
+            won = dungeon.moveRight();
+            break;
+        }
+    } while (!won);
+
+    clearScreen(); // std::cout << std::endl;
+    dungeon.printVisibleAreaTo(std::cout);
+
+    std::cout << "congratulations you escaped the maze!";
+    getCharacter(); //std::cin.get();
+
     return 0;
 }
---
+++ solutions/dungeon_escape/vector2d.hpp
@@ -0,0 +1,94 @@
+#pragma once
+#include <cmath>
+
+struct Vector2D
+{
+    int x, y;
+
+    Vector2D &operator+=(Vector2D rhs)
+    {
+        x += rhs.x;
+        y += rhs.y;
+        return *this;
+    }
+
+    Vector2D &operator-=(Vector2D rhs)
+    {
+        x -= rhs.x;
+        y -= rhs.y;
+        return *this;
+    }
+
+    Vector2D &operator*=(Vector2D rhs)
+    {
+        x *= rhs.x;
+        y *= rhs.y;
+        return *this;
+    }
+
+    Vector2D &operator+=(int rhs)
+    {
+        x += rhs;
+        y += rhs;
+        return *this;
+    }
+
+    Vector2D &operator-=(int rhs)
+    {
+        x -= rhs;
+        y -= rhs;
+        return *this;
+    }
+
+    Vector2D &operator*=(int rhs)
+    {
+        x *= rhs;
+        y *= rhs;
+        return *this;
+    }
+};
+
+Vector2D operator+(Vector2D lhs, Vector2D rhs)
+{
+    return {lhs.x + rhs.x, lhs.y + rhs.y};
+}
+
+Vector2D operator-(Vector2D lhs, Vector2D rhs)
+{
+    return {lhs.x - rhs.x, lhs.y - rhs.y};
+}
+
+Vector2D operator*(Vector2D lhs, Vector2D rhs)
+{
+    return {lhs.x * rhs.x, lhs.y * rhs.y};
+}
+
+Vector2D operator+(int lhs, Vector2D rhs)
+{
+    return {lhs + rhs.x, lhs + rhs.y};
+}
+
+Vector2D operator-(int lhs, Vector2D rhs)
+{
+    return {lhs - rhs.x, lhs - rhs.y};
+}
+
+Vector2D operator*(int lhs, Vector2D rhs)
+{
+    return {lhs * rhs.x, lhs * rhs.y};
+}
+
+Vector2D operator+(Vector2D lhs, int rhs)
+{
+    return {lhs.x + rhs, lhs.y + rhs};
+}
+
+Vector2D operator-(Vector2D lhs, int rhs)
+{
+    return {lhs.x - rhs, lhs.y - rhs};
+}
+
+Vector2D operator*(Vector2D lhs, int rhs)
+{
+    return {lhs.x * rhs, lhs.y * rhs};
+}
